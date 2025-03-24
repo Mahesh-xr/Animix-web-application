7c85e3b1178161cef746136f73eb0ef7460ac3a2
Mahesh-xr <mahesh.572xr@gmail.com>
2025-03-24 18:59:24 +0530

    add to fav pages and initial implementaion add

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/search', async (req, res) => {
     }
   });
   
-  app.get('/view/:mal_id', async (req, res) => {
+app.get('/view/:mal_id', async (req, res) => {
     const mal_id = req.params.mal_id; // Extract mal_id from the URL
     const API_URL = `https://api.jikan.moe/v4/anime/${mal_id}`; // Jikan API endpoint for fetching specific anime
     const CHARACTER_API_URL = `https://api.jikan.moe/v4/anime/${mal_id}/characters`
@@ -52,7 +52,6 @@ app.get('/search', async (req, res) => {
         const animeData = response.data.data; // Get the anime data
         const char_res  = await axios.get(CHARACTER_API_URL);
         const characterData = char_res.data.data;
-        console.log(characterData)
         
         // Render the anime detail page with the fetched data
         res.render('view.ejs', {
@@ -64,6 +63,32 @@ app.get('/search', async (req, res) => {
         res.status(500).send('Error fetching anime details');
     }
 });
+
+app.get('/addToFav/:mal_id', async (req, res) => {
+  const mal_id = req.params.mal_id; // Extract mal_id from the URL
+  console.log("Anime mal_id : "+mal_id)
+  const API_URL = `https://api.jikan.moe/v4/anime/${mal_id}`; // Jikan API endpoint for fetching specific anime
+  const CHARACTER_API_URL = `https://api.jikan.moe/v4/anime/${mal_id}/characters`
+  
+  try {
+      // Make a request to the Jikan API
+      const response = await axios.get(API_URL);
+      const animeData = response.data.data; // Get the anime data
+      
+      
+      // Render the anime detail page with the fetched data
+      res.render('addToFav.ejs', {
+        anime: animeData,
+      })
+  } catch (error) {
+    console.log(error)
+      res.status(500).send('Error fetching anime details');
+  }
+});
+
+app.post("add-to-favorites",(req, res)=>{
+  
+})
 
 
 app.listen(PORT, ()=>{
